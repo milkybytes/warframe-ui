b34a381813f533438849b031b1429df8561f9f91
milkybytes <milkietoast@gmail.com>
2026-03-27 17:58:45 -0700

    Adding use-client banner

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,9 @@ export default defineConfig({
     },
     rollupOptions: {
       external: ['react', 'react-dom', 'react/jsx-runtime'],
+      output: {
+        banner: "'use client';",
+      },
     },
     cssCodeSplit: false,
   },
